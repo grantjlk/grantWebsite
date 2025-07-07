45a30a755ce63f23012024c1c86917e10dd9fb4c
Grant LeRette <grantjlk@gmail.com>
2025-07-06 20:06:15 -0500

    updated home layout and background

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ const navLinks = document.getElementById('navLinks');
 toggleButton.addEventListener('click', () => {
   sidebar.classList.toggle('expanded');
 });
+
+// hover spotlight effect
+document.addEventListener('mousemove', (e) => {
+  const x = `${e.clientX}px`;
+  const y = `${e.clientY}px`;
+  document.documentElement.style.setProperty('--x', x);
+  document.documentElement.style.setProperty('--y', y);
+});
