ce5183e7a89b6bb81214de06e01d51e698c29837
Grant LeRette <grantjlk@gmail.com>
2025-07-07 18:27:36 -0500

    made basic personal page with images as links to other parts of the website

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 // Fetch the sidebar file
 fetch('sidebar.html')
-  .then(res => res.text())        
-  .then(html => {
-    // insert sidebar html into the placeholder
-    document.getElementById('sidebar-placeholder').innerHTML = html;
+    .then(res => res.text())        
+    .then(html => {
+        // insert sidebar html into the placeholder
+        document.getElementById('sidebar-placeholder').innerHTML = html;
 
-    // sidebar toggle behavior after loaded
-    const toggleButton = document.getElementById('toggleSidebar');
-    const sidebar = document.getElementById('sidebar');
-    const navLinks = document.getElementById('navLinks');
+        // sidebar toggle behavior after loaded
+        const toggleButton = document.getElementById('toggleSidebar');
+        const sidebar = document.getElementById('sidebar');
+        const navLinks = document.getElementById('navLinks');
 
-    toggleButton.addEventListener('click', () => {
-    sidebar.classList.toggle('expanded');
+        toggleButton.addEventListener('click', () => {
+        sidebar.classList.toggle('expanded');
+        });
     });
-  });
 
 
 
-// hover spotlight effect
-document.addEventListener('mousemove', (e) => {
-  const x = `${e.clientX}px`;
-  const y = `${e.clientY}px`;
-  document.documentElement.style.setProperty('--x', x);
-  document.documentElement.style.setProperty('--y', y);
-});
+// hover spotlight effect, if on homepage
+if (window.location.pathname.endsWith('index.html')){
+    document.addEventListener('mousemove', (e) => {
+	    const x = `${e.clientX}px`;
+        const y = `${e.clientY}px`;
+        document.documentElement.style.setProperty('--x', x);
+        document.documentElement.style.setProperty('--y', y);
+    });
+}
